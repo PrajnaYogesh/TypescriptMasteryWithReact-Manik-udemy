38d4a88e737b40b1205b4cfa060277b90e7f0891
Prajna <prajna.yogish4@gmail.com>
2025-02-15 21:21:25 -0500

    created watch script for auto compile, basic TS code

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+const students = [{
+        name: "John",
+        score: 70,
+    },
+    {
+        name: "Mark",
+        score: 90,
+    }];
+for (const student of students) {
+    console.log("Name:", student.name);
+    console.log("Score:", student.score);
+}
